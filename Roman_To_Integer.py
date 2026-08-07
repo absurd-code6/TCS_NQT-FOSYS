445de8630c9de2b910f2c44d8e8339e6795cadc8
absurd-code6 <variable3.5turbo@gmail.com>
2026-08-07 16:29:44 +0530

    Add files via upload

---
+++ Roman_To_Integer.py
@@ -0,0 +1,44 @@
+'''Roman numerals are represented by seven different symbols: 
+I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones 
+added together.12 is written as XII, which is simply X + II. 
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. 
+However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. 
+Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. 
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+'''
+
+def Roman_to_Int(num):
+    roman_letters={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+# Start with the value of the first Roman numeral
+    Int=roman_letters[num[0]] # Impt
+    for i in range(1,len(num)):
+        if num[i] in roman_letters:
+            Int+=roman_letters[num[i]]
+#If a smaller numeral precedes a larger one (e.g., IV or IX),
+#subtract twice the previous value (since it was added earlier)
+        if roman_letters[num[i]]>roman_letters[num[i-1]]:
+            Int-=2*roman_letters[num[i-1]]
+    return Int
+
+num=input().strip()
+print(Roman_to_Int(num))
+           
